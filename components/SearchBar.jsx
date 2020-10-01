4f01e43b0ecbe98cdf67b23f7d01928ef13853f0
Irene-24 <okaforirenen@gmail.com>
2020-10-01 12:53:45 +0100

    homescreen basics

---
+++ components/SearchBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const SearchBar = () =>
+{
+    return (
+        <View>
+            <Text>search</Text>
+        </View>
+    );
+};
+
+export default SearchBar;
+
+const styles = StyleSheet.create( {} );
