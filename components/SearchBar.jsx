30076f0ff137855c504b836e95aadad3c2acd00c
Irene-24 <okaforirenen@gmail.com>
2020-10-01 14:42:13 +0100

    working on searchbar

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,15 +1,49 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
 const SearchBar = () =>
 {
     return (
-        <View>
-            <Text>search</Text>
+        <View style={ styles.Wrapper }>
+            <FontAwesome
+                styles={ styles.Icon }
+                name="search"
+                size={ 24 }
+                color="white" />
+            <TextInput
+                style={ styles.Input }
+                placeholder="Enter search term..."
+                clearButtonMode="always"
+                returnKeyType="search"
+                clearButtonMode="always"
+            />
         </View>
     );
 };
 
 export default SearchBar;
 
-const styles = StyleSheet.create( {} );
+const styles = StyleSheet.create(
+    {
+        Wrapper:
+        {
+            backgroundColor: "coral",
+            height: 40,
+            borderRadius: 5,
+            marginHorizontal: 20,
+            marginVertical: 5,
+            padding: 5,
+            flexDirection: "row",
+        },
+        Input:
+        {
+            flex: 1,
+            color: "#fff",
+            marginLeft: 10
+        },
+        Icon:
+        {
+            alignSelf: "center"
+        }
+    } );
--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SearchBar from "../components/SearchBar";
 
 const HomeScreen = () =>
 {
     return (
         <View>
+            <SearchBar />
             <Text>Home</Text>
         </View>
     );
